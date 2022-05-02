991e253033610ef7db18112416eb6f95a1e78266
agronomi7 <agroni.nomi@gmail.com>
2022-05-03 00:23:33 +0200

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
-import { Component, ElementRef, ViewChild, ViewContainerRef } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  ViewChildren,
+  ViewContainerRef,
+} from '@angular/core';
 import { NgxMoveableComponent } from 'ngx-moveable';
-import { Frame } from "scenejs";
+import { Frame } from 'scenejs';
 
 @Component({
   selector: 'app-root',
@@ -8,133 +14,137 @@ import { Frame } from "scenejs";
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
- // resize
- @ViewChild("target", { static: false }) target: ElementRef;
- @ViewChild("label", { static: false }) label: ElementRef;
- @ViewChild("moveable", { static: false }) moveable: NgxMoveableComponent;
- scalable = true;
- resizable = false;
- warpable = false;
- frame = new Frame({
-   width: "250px",
-   height: "200px",
-   left: "100px",
-   top: "100px",
-   transform: {
-     rotate: "0deg",
-     scaleX: 1,
-     scaleY: 1,
-     matrix3d: [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
-   }
- });
- //resize
+  // resize
+  @ViewChildren('target') target: ElementRef;
+  @ViewChild('label', { static: false }) label: ElementRef;
+  @ViewChild('moveable', { static: false }) moveable: NgxMoveableComponent;
 
- constructor(public vcRef: ViewContainerRef) {}
- ngOnDestroy(): void {
-   // window.removeEventListener("resize", this.onWindowReisze);
- }
- ngOnInit(): void {
-   //frame
-   // window.addEventListener("resize", this.onWindowReisze);
-   //frame
- }
+  test = false;
 
- clickScalable() {
-   this.scalable = true;
-   this.resizable = false;
-   this.warpable = false;
- }
- clickResizable() {
-   this.scalable = false;
-   this.resizable = true;
-   this.warpable = false;
- }
- clickWarpable() {
-   this.scalable = false;
-   this.resizable = false;
-   this.warpable = true;
- }
- setTransform(target) {
-   target.style.cssText = this.frame.toCSS();
- }
- setLabel(clientX, clientY, text) {
-   this.label.nativeElement.style.cssText = `
-display: block; transform: translate(${clientX}px, ${clientY -
-     10}px) translate(-100%, -100%) translateZ(-100px);`;
-   this.label.nativeElement.innerHTML = text;
- }
- onPinch({ target, clientX, clientY }) {
-   setTimeout(() => {
-     this.setLabel(
-       clientX,
-       clientY,
-       `X: ${this.frame.get("left")}
- <br/>Y: ${this.frame.get("top")}
- <br/>W: ${this.frame.get("width")}
- <br/>H: ${this.frame.get("height")}
- <br/>S: ${this.frame.get("transform", "scaleX").toFixed(2)}, ${this.frame
-         .get("transform", "scaleY")
-         .toFixed(2)}
- <br/>R: ${parseFloat(this.frame.get("transform", "rotate")).toFixed(1)}deg
+  scalable = true;
+  resizable = false;
+  warpable = false;
+  frame = new Frame({
+    width: '250px',
+    height: '200px',
+    left: '100px',
+    top: '100px',
+    transform: {
+      rotate: '0deg',
+      scaleX: 1,
+      scaleY: 1,
+      matrix3d: [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+    },
+  });
+  //resize
+
+  constructor(public vcRef: ViewContainerRef) {}
+  ngOnDestroy(): void {
+    // window.removeEventListener("resize", this.onWindowReisze);
+  }
+  ngOnInit(): void {
+    //frame
+    // window.addEventListener("resize", this.onWindowReisze);
+    //frame
+  }
+
+  clickScalable() {
+    this.scalable = true;
+    this.resizable = false;
+    this.warpable = false;
+  }
+  clickResizable() {
+    this.scalable = false;
+    this.resizable = true;
+    this.warpable = false;
+  }
+  clickWarpable() {
+    this.scalable = false;
+    this.resizable = false;
+    this.warpable = true;
+  }
+  setTransform(target) {
+    target.style.cssText = this.frame.toCSS();
+  }
+  setLabel(clientX, clientY, text) {
+    this.label.nativeElement.style.cssText = `
+display: block; transform: translate(${clientX}px, ${
+      clientY - 10
+    }px) translate(-100%, -100%) translateZ(-100px);`;
+    this.label.nativeElement.innerHTML = text;
+  }
+  onPinch({ target, clientX, clientY }) {
+    setTimeout(() => {
+      this.setLabel(
+        clientX,
+        clientY,
+        `X: ${this.frame.get('left')}
+ <br/>Y: ${this.frame.get('top')}
+ <br/>W: ${this.frame.get('width')}
+ <br/>H: ${this.frame.get('height')}
+ <br/>S: ${this.frame.get('transform', 'scaleX').toFixed(2)}, ${this.frame
+          .get('transform', 'scaleY')
+          .toFixed(2)}
+ <br/>R: ${parseFloat(this.frame.get('transform', 'rotate')).toFixed(1)}deg
  `
-     );
-   });
- }
- onDrag({ target, clientX, clientY, top, left, isPinch }) {
-   this.frame.set("left", `${left}px`);
-   this.frame.set("top", `${top}px`);
-   this.setTransform(target);
-   if (!isPinch) {
-     this.setLabel(clientX, clientY, `X: ${left}px<br/>Y: ${top}px`);
-   }
- }
- onScale({ target, delta, clientX, clientY, isPinch }) {
-   const scaleX = this.frame.get("transform", "scaleX") * delta[0];
-   const scaleY = this.frame.get("transform", "scaleY") * delta[1];
-   this.frame.set("transform", "scaleX", scaleX);
-   this.frame.set("transform", "scaleY", scaleY);
-   this.setTransform(target);
-   if (!isPinch) {
-     this.setLabel(
-       clientX,
-       clientY,
-       `S: ${scaleX.toFixed(2)}, ${scaleY.toFixed(2)}`
-     );
-   }
- }
- onRotate({ target, clientX, clientY, beforeDelta, isPinch }) {
-   const deg = parseFloat(this.frame.get("transform", "rotate")) + beforeDelta;
+      );
+    });
+  }
+  onDrag({ target, clientX, clientY, top, left, isPinch }) {
+    this.frame.set('left', `${left}px`);
+    this.frame.set('top', `${top}px`);
+    this.setTransform(target);
+    if (!isPinch) {
+      this.setLabel(clientX, clientY, `X: ${left}px<br/>Y: ${top}px`);
+    }
+  }
+  onScale({ target, delta, clientX, clientY, isPinch }) {
+    const scaleX = this.frame.get('transform', 'scaleX') * delta[0];
+    const scaleY = this.frame.get('transform', 'scaleY') * delta[1];
+    this.frame.set('transform', 'scaleX', scaleX);
+    this.frame.set('transform', 'scaleY', scaleY);
+    this.setTransform(target);
+    if (!isPinch) {
+      this.setLabel(
+        clientX,
+        clientY,
+        `S: ${scaleX.toFixed(2)}, ${scaleY.toFixed(2)}`
+      );
+    }
+  }
+  onRotate({ target, clientX, clientY, beforeDelta, isPinch }) {
+    const deg = parseFloat(this.frame.get('transform', 'rotate')) + beforeDelta;
 
-   this.frame.set("transform", "rotate", `${deg}deg`);
-   this.setTransform(target);
-   if (!isPinch) {
-     this.setLabel(clientX, clientY, `R: ${deg.toFixed(1)}`);
-   }
- }
- onResize({ target, clientX, clientY, width, height, isPinch }) {
-   this.frame.set("width", `${width}px`);
-   this.frame.set("height", `${height}px`);
-   this.setTransform(target);
-   if (!isPinch) {
-     this.setLabel(clientX, clientY, `W: ${width}px<br/>H: ${height}px`);
-   }
- }
- onWarp({ target, clientX, clientY, delta, multiply }) {
-   this.frame.set(
-     "transform",
-     "matrix3d",
-     multiply(this.frame.get("transform", "matrix3d"), delta)
-   );
-   this.setTransform(target);
-   this.setLabel(clientX, clientY, `X: ${clientX}px<br/>Y: ${clientY}px`);
- }
- onEnd() {
-   this.label.nativeElement.style.display = "none";
- }
+    this.frame.set('transform', 'rotate', `${deg}deg`);
+    this.setTransform(target);
+    if (!isPinch) {
+      this.setLabel(clientX, clientY, `R: ${deg.toFixed(1)}`);
+    }
+  }
+  onResize({ target, clientX, clientY, width, height, isPinch }) {
+    this.frame.set('width', `${width}px`);
+    this.frame.set('height', `${height}px`);
+    this.setTransform(target);
+    if (!isPinch) {
+      this.setLabel(clientX, clientY, `W: ${width}px<br/>H: ${height}px`);
+    }
+  }
+  onWarp({ target, clientX, clientY, delta, multiply }) {
+    this.frame.set(
+      'transform',
+      'matrix3d',
+      multiply(this.frame.get('transform', 'matrix3d'), delta)
+    );
+    this.setTransform(target);
+    this.setLabel(clientX, clientY, `X: ${clientX}px<br/>Y: ${clientY}px`);
+  }
+  onEnd() {
+    this.label.nativeElement.style.display = 'none';
+  }
 
- //frame
- // onWindowReisze = () => {
- //   this.moveable.updateRect();
- // };
- //frame
+  //frame
+  // onWindowReisze = () => {
+  //   this.moveable.updateRect();
+  // };
+  //frame
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { NgxMoveableModule } from "ngx-moveable";
-import { AppComponent } from "./app.component";
-import { NgxGuidelinesComponent } from "../ngx-guidelines/ngx-guidelines.component";
-import { NgxGuidelinesModule } from "../ngx-guidelines/ngx-guidelines.module";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgxMoveableModule } from 'ngx-moveable';
+import { AppComponent } from './app.component';
+import { NgxGuidelinesComponent } from '../ngx-guidelines/ngx-guidelines.component';
+import { NgxGuidelinesModule } from '../ngx-guidelines/ngx-guidelines.module';
+import { HelloComponent } from './hello.component';
 
 @NgModule({
-  declarations: [AppComponent, NgxGuidelinesComponent],
+  declarations: [AppComponent, NgxGuidelinesComponent, HelloComponent],
   imports: [BrowserModule, NgxMoveableModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
